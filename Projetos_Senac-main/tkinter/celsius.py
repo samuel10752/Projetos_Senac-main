c0fdac0b99380e81cc3f2dfe91e5b0da9b6a3548
Fulano de Tal <fulanodetal@exemplo.br>
2022-06-06 10:54:54 -0300

    Atualização do dia 06/06/2022, Colocação do frame

--- Projetos_Senac-main/tkinter/celsius.py
+++ Projetos_Senac-main/tkinter/celsius.py
@@ -1,26 +1,32 @@
 from tkinter import *
 #back-end
 def calculo():
-    if in0.get().replace('.','',1).isdigit():
-        lb1['text']=(float(in0.get())*1.8)+32      
+    if in0_fr1.get().replace('.','',1).isdigit():
+        lb1_fr2['text']=(float(in0_fr1.get())*1.8)+32      
         
           
 
 #front-end
 root = Tk()
-root.grid_rowconfigure(0, weight=1)
-root.grid_rowconfigure(1, weight=1)
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=1)
-#widgets
-lb0 = Label(root, text='Insira Cº', font='Arial 25')
-in0 = Entry(root, font='Arial 25')
-bt0 = Button(root, text='Obtenha F', font='Arial 25', command=calculo)
-lb1 = Label(root, text='Resultado', font='Arial 25')
 
-lb0.grid(row=0, column=0, sticky=NSEW)
-in0.grid(row=0, column=1, sticky=NSEW)
-bt0.grid(row=1, column=0, sticky=NSEW)
-lb1.grid(row=1, column=1, sticky=NSEW)
+root.geometry('500x100') # declara o tamanho da jenela (400x300) tamanho inicial/ +100 +100 lugar onde ela vai abrir 
+
+fr1 = Frame(root, background='black')
+fr2 = Frame(root, background='black')
+
+#Frame
+fr1.pack()
+fr2.pack()
+
+#widgets
+lb0_fr1 = Label(fr1,text='Insira Cº', font='Arial 25')
+in0_fr1 = Entry(fr1, font='Arial 25')
+bt0_fr2 = Button(fr2, text='Obtenha F', font='Arial 25', command=calculo)
+lb1_fr2 = Label(fr2, text='Resultado', font='Arial 25')
+
+lb0_fr1.grid(row=0, column=0, sticky=NSEW)
+in0_fr1.grid(row=0, column=1, sticky=NSEW)
+bt0_fr2.grid(row=1, column=0, sticky=NSEW)
+lb1_fr2.grid(row=1, column=1, sticky=NSEW)
 
 root.mainloop()
