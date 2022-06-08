30d3cc811f30de0b8c537113fce9477d012577bd
Fulano de Tal <fulanodetal@exemplo.br>
2022-06-08 09:37:51 -0300

    Atualização do dia 08/06/2022 09:38

--- Projetos_Senac-main/tkinter/login.py
+++ Projetos_Senac-main/tkinter/login.py
@@ -1,10 +1,12 @@
 from cgitb import text
 from tkinter import*
 from tkinter.ttk import Style
+from datetime import datetime
+     
 
 #janela 
 root = Tk()
-root.geometry('790x293+720+400') 
+root.geometry('630x240+720+400') 
 
 
 fr1 = LabelFrame(root, background='#ededed')
@@ -12,11 +14,11 @@ fr2 = LabelFrame(root, background='#ededed')
 fr3 = Frame(root, background='#ededed')
 
 #Dados Pessoais
-Title_fr1 = Label (fr1, text='Dados Pessoais', font='Sans-Serif  20', fg='#993399')
+Title_fr1 = Label (fr1, text='Dados Pessoais', font='Sans-Serif  20', fg='#6495ED')
 
 #linha 1
-Nome_fr1 = Label (fr1, text='Nome:', font="Sans-Serif  15", anchor=E)
-Nomeinput_fr1 = Entry (fr1, font='Sans-Serif ')
+Nome_fr1 = Label (fr1, text='Nome:', font="Sans-Serif  15")
+Nomeinput_fr1 = Entry (fr1, font='Sans-Serif ', width=30)
 
 #Linha 2
 DataNasc_fr1 = Label (fr1, text='Data Nasc:', font="Sans-Serif  15")
@@ -24,7 +26,7 @@ Datainput_fr1 = Entry (fr1, font='Sans-Serif  ')
 
 #Linha 3
 CPF_fr1 = Label (fr1, text='CPF:', font="Sans-Serif  15")
-CPFinput_fr1 = Entry (fr1, font='Sans-Serif  ')
+CPFinput_fr1 = Entry (fr1, font='Sans-Serif  ', width=11)
 
 #Linha 4
 Telefone_fr1 = Label (fr1, text='Telefone:', font="Sans-Serif  15")
@@ -35,15 +37,15 @@ Telefoneinput_fr1 = Entry (fr1, font='Sans-Serif  ')
 
 #Linha 5
 
-Title2_fr2 = Label (fr2, text='Endereço', font='Sans-Serif  20', fg='#993399')
+Title2_fr2 = Label (fr2, text='Endereço', font='Sans-Serif  20', fg='#6495ED')
 
 #linha 6
 Rua_fr2 = Label (fr2, text='Rua:', font="Sans-Serif  15")
-Ruainput_fr2 = Entry (fr2, font='Sans-Serif ')
+Ruainput_fr2 = Entry (fr2, font='Sans-Serif ', width=25)
 
 #Linha 7
 Bairro_fr2 = Label (fr2, text='Bairro:', font="Sans-Serif  15")
-Bairroinput_fr2 = Entry (fr2, font='Sans-Serif ')
+Bairroinput_fr2 = Entry (fr2, font='Sans-Serif ', width=25)
 
 
 #Linha 8
@@ -54,7 +56,7 @@ Cidadeinput_fr2 = Entry (fr2, font='Sans-Serif ')
 #Linha 9
 
 N_fr2 = Label (fr2, text='N°:', font="Sans-Serif  15")
-Ninput_fr2 = Entry (fr2, font='Sans-Serif ')
+Ninput_fr2 = Entry (fr2, font='Sans-Serif ', width=4)
 
 #Linha 10
 
@@ -68,57 +70,59 @@ Gravar_fr3 = Button(fr3, text='Gravar Dados', font='Sans-Serif  15')
 imprimir_fr3 = Button(fr3,text='Imprimir dados', font='Sans-Serif  15')
 
 #Frame
-fr1.grid(sticky=W)
+fr1.grid()
 fr2.grid()
-fr3.grid(sticky=W)
+fr3.grid()
 
 
 # Organizar os Widgets 
 
+
 #Titulo 
-Title_fr1.grid(row=0,  column=0, sticky=NSEW)
+Title_fr1.grid(row=0,  column=1, sticky=EW, padx=0)
 
 #Nome
-Nome_fr1.grid(row=1, column=0,sticky=EW, padx=10)
-Nomeinput_fr1.grid(row=1, column=1, sticky=EW, padx=10)
+Nome_fr1.grid(row=1, column=0,sticky=E)
+Nomeinput_fr1.grid(row=1, column=1, sticky=E, padx=0)
 
 #Data Nasc
-DataNasc_fr1.grid(row=2, column=0,  sticky=W, padx=10)
-Datainput_fr1.grid(row=2, column=1,  sticky=W, padx=10)
+DataNasc_fr1.grid(row=1, column=3,  sticky=NSEW)
+Datainput_fr1.grid(row=1, column=4,  sticky=E, padx=0)
+
 #CPf
-CPF_fr1.grid(row=3, column=0,  sticky=W, padx=10)
-CPFinput_fr1.grid(row=3, column=1,  sticky=W, padx=10)
+CPF_fr1.grid(row=3, column=0,  sticky=E)
+CPFinput_fr1.grid(row=3, column=1,  sticky=W, padx=00)
 
 #Telefone
-Telefone_fr1.grid(row=4, column=0,  sticky=W, padx=10)
-Telefoneinput_fr1.grid(row=4,column=1,  sticky=W, padx=10)
+Telefone_fr1.grid(row=3, column=3, sticky=W)
+Telefoneinput_fr1.grid(row=3,column=4, sticky=W, padx=0)
 
 
 #Endereço
 
 #Titulo 
-Title2_fr2.grid(row=0, column=0,sticky=NSEW)
+Title2_fr2.grid(row=0, column=1,sticky=E, padx=0)
 
 #Rua
-Rua_fr2.grid(row=1, column=0,sticky=NSEW)
-Ruainput_fr2.grid(row=1, column=1,columnspan=3,sticky=NSEW)
+Rua_fr2.grid(row=1, column=0, sticky=E)
+Ruainput_fr2.grid(row=1, column=1, sticky=E, padx=0)
 
 #Bairro
-Bairro_fr2.grid(row=2, column=0, sticky=NSEW)
-Bairroinput_fr2.grid(row=2, column=1, sticky=NSEW)
+Bairro_fr2.grid(row=2, column=0, sticky=E,padx=0)
+Bairroinput_fr2.grid(row=2, column=1, sticky=E, padx=0)
 
 #Cidade
 
-Cidade_fr2.grid(row=2, column=2,sticky=NSEW)
-Cidadeinput_fr2.grid(row=2, column=3,sticky=NSEW) 
+Cidade_fr2.grid(row=1, column=2,sticky=W,padx=0)
+Cidadeinput_fr2.grid(row=1, column=3,sticky=W, padx=0) 
 
 #N°
-N_fr2.grid(row=1, column=4, sticky=NSEW)
+N_fr2.grid(row=1, column=4, sticky=E)
 Ninput_fr2.grid(row=1, column=5, sticky=NSEW)
 
 #UF
-UF_fr2.grid(row=2, column=4, sticky=NSEW)
-UFinput_fr2.grid(row=2, column=5, sticky=NSEW)
+UF_fr2.grid(row=2, column=2, sticky=E)
+UFinput_fr2.grid(row=2, column=3, sticky=NSEW)
 
 #Input
 Gravar_fr3.grid(row=3, column=7, sticky=NSEW)
