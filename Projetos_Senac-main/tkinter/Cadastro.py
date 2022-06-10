207af5da21cc27af5ff4c239171aa5bd6172452f
Samuel <samuelcauabarbosa114@gmail.com>
2022-06-09 23:44:26 -0300

    Novas funçoes 09/06 23:44

--- Projetos_Senac-main/tkinter/Cadastro.py
+++ Projetos_Senac-main/tkinter/Cadastro.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 #Função / Data Nasc
 
-def capitura(event=None):    
+def data(event=None):    
     x=Datainput_fr1.get().replace('/','')[:8]
     y=''
     if event.keysym.lower() == "backspace": return
@@ -20,16 +20,53 @@ def capitura(event=None):
     Datainput_fr1.insert(0, y)
 #CRÉDITO ao JeanExtreme002. Resposta disponível em: https://pt.stackoverflow.com/questions/492705/criando-um-entry-formatado-para-cpf-em-python-tkinter#:~:text=Para%20formatar%20o%20CPF%20enquanto,e%20a%20fun%C3%A7%C3%A3o%20de%20formata%C3%A7%C3%A3o.
 
+#Verificador de CPF
+
+def CPf(event=None):    
+    x=CPFinput_fr1.get().replace('.','').replace('-', '')[:11]
+    y=''
+    if event.keysym.lower() == "backspace": return
+    for i in range(len(x)):
+        if not x[i] in '0123456789': continue
+        if i in [2,5]:
+            y+=x[i] + '.'
+        elif i == 8:
+            y+=x[i] + '-'
+        else:
+            y+=x[i]
+    CPFinput_fr1.delete(0, 'end')
+    CPFinput_fr1.insert(0, y)
+
+#Vericficador de telefone
+
+def telefone(event=None):    
+    x=Telefoneinput_fr1.get().replace('(','').replace(')', '').replace('-','')[:11]
+    y=''
+    if event.keysym.lower() == "backspace": return
+    for i in range(len(x)):
+        if not x[i] in '00123456789': continue
+        if i in []:
+            y+=x[i] - '('
+        if i in [1]:
+            y+=x[i] + ')'   
+        elif i == 6:
+            y+=x[i] + '-'
+        else:
+            y+=x[i]
+    Telefoneinput_fr1.delete(0, 'end')
+    Telefoneinput_fr1.insert(0, y)
+
+    
 
 #janela 
 root = Tk()
-root.geometry('630x240+720+400') 
+root.geometry('780x240+720+400') 
 fr1 = LabelFrame(root, background='#ededed')
 fr2 = LabelFrame(root, background='#ededed')
 fr3 = Frame(root, background='#ededed')
-
+#ededed
 #Dados Pessoais
-Title_fr1 = Label (fr1, text='Dados Pessoais', font='Sans-Serif  20', fg='#6495ED')
+Title_fr1 = Label (fr1, text='Dados Pessoais', font='Sans-Serif  20')
 
 #linha 1
 Nome_fr1 = Label (fr1, text='Nome:', font="Sans-Serif  15")
@@ -37,23 +74,25 @@ Nomeinput_fr1 = Entry (fr1, font='Sans-Serif ', width=30)
 
 #Linha 2
 DataNasc_fr1 = Label (fr1, text='Data Nasc:', font="Sans-Serif  15")
-Datainput_fr1 = Entry (fr1, font='Sans-Serif  ')
-Datainput_fr1.bind("<KeyRelease>", capitura)
+Datainput_fr1 = Entry (fr1, font='Sans-Serif  ',width=9)
+Datainput_fr1.bind("<KeyRelease>", data)
 
 #Linha 3
 CPF_fr1 = Label (fr1, text='CPF:', font="Sans-Serif  15")
-CPFinput_fr1 = Entry (fr1, font='Sans-Serif  ', width=11)
+CPFinput_fr1 = Entry (fr1, font='Sans-Serif  ', width=13)
+CPFinput_fr1.bind("<KeyRelease>", CPf)
 
 #Linha 4
 Telefone_fr1 = Label (fr1, text='Telefone:', font="Sans-Serif  15")
-Telefoneinput_fr1 = Entry (fr1, font='Sans-Serif  ')
+Telefoneinput_fr1 = Entry (fr1, font='Sans-Serif  ',width=18)
+Telefoneinput_fr1.bind("<KeyRelease>", telefone)
 
 
 #Endereço
 
 #Linha 5
 
-Title2_fr2 = Label (fr2, text='Endereço', font='Sans-Serif  20', fg='#6495ED')
+Title2_fr2 = Label (fr2, text='Endereço', font='Sans-Serif  20')
 
 #linha 6
 Rua_fr2 = Label (fr2, text='Rua:', font="Sans-Serif  15")
@@ -77,13 +116,13 @@ Ninput_fr2 = Entry (fr2, font='Sans-Serif ', width=4)
 #Linha 10
 
 UF_fr2 = Label (fr2, text='UF:', font="Sans-Serif  15")
-UFinput_fr2 = Entry (fr2, font='Sans-Serif ')
+UFinput_fr2 = Entry (fr2, font='Sans-Serif ',width=3)
 
 
 
 ############################Input#####################
-Gravar_fr3 = Button(fr3, text='Gravar Dados', font='Sans-Serif  15')
-imprimir_fr3 = Button(fr3,text='Imprimir dados', font='Sans-Serif  15')
+Gravar_fr3 = Button(fr3, text='Gravar Dados', font='Sans-Serif  15',bg='green')
+imprimir_fr3 = Button(fr3,text='Imprimir dados', font='Sans-Serif  15',bg='green')
 
 #Frame
 fr1.grid()
@@ -102,8 +141,8 @@ Nome_fr1.grid(row=1, column=0,sticky=E)
 Nomeinput_fr1.grid(row=1, column=1, sticky=E, padx=0)
 
 #Data Nasc
-DataNasc_fr1.grid(row=1, column=3,  sticky=NSEW)
-Datainput_fr1.grid(row=1, column=4,  sticky=E, padx=0)
+DataNasc_fr1.grid(row=1, column=3, sticky=NSEW)
+Datainput_fr1.grid(row=1, column=4, sticky=W)
 
 #CPf
 CPF_fr1.grid(row=3, column=0,  sticky=E)
@@ -134,11 +173,11 @@ Cidadeinput_fr2.grid(row=1, column=3,sticky=W, padx=0)
 
 #N°
 N_fr2.grid(row=1, column=4, sticky=E)
-Ninput_fr2.grid(row=1, column=5, sticky=NSEW)
+Ninput_fr2.grid(row=1, column=5, sticky=W)
 
 #UF
 UF_fr2.grid(row=2, column=2, sticky=E)
-UFinput_fr2.grid(row=2, column=3, sticky=NSEW)
+UFinput_fr2.grid(row=2, column=3, sticky=W)
 
 #Input
 Gravar_fr3.grid(row=3, column=7, sticky=NSEW)
