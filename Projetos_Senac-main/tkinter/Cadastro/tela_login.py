436647c5fb4541a88d6e5af4ec26b4ea97a03e27
Samuel <samuelcauabarbosa114@gmail.com>
2022-06-13 13:07:21 -0300

    atualização do 13:07

--- Projetos_Senac-main/tkinter/Cadastro/tela_login.py
+++ Projetos_Senac-main/tkinter/Cadastro/tela_login.py
@@ -7,8 +7,8 @@ root.title("Login de Funcionario ")
 fr1 = Frame(root)
 
 #geometria
-root.geometry('400x78+720+400')
-root.resizable(width=False, height=False)
+root.geometry('380x150+720+400')
+#root.resizable(width=False, height=False)
 
 #criar os widgets
 
@@ -16,8 +16,12 @@ root.resizable(width=False, height=False)
 lb0_fr1 = Label(fr1, text='Usuario:', font="Arial 20")
 lb1_fr1 = Label(fr1, text='Senha:', font="Arial 20")
 
-#--Frame 2 ---
-
+#--Entrada ---
+lb2_fr1 = Entry(fr1, font='Arial 18')
+lb3_fr1 = Entry(fr1, font='Arial 18')
+#--Button ---
+tb0_fr1 = Button(fr1,text='Entrar', font="Arial 20",width=5)
+tb1_fr1 = Button(fr1, text='Voltar', font="Arial 20",width=5)
 
 #---Configuração do Frame---
 fr1.grid()
@@ -30,7 +34,12 @@ fr1.grid()
 lb0_fr1.grid(row=0, column=2,sticky=W, padx=5)
 lb1_fr1.grid(row=1, column=2,sticky=W, padx=5)
 
-#---Frame 2---
+#--Entrada --
+lb2_fr1.grid(row=0,column=2,sticky=W,padx=110)
+lb3_fr1.grid(row=1,column=2,sticky=W,padx=110)
 
-#executar a janela
+#---Butoon Função---
+tb0_fr1.grid(row=3, column=2, sticky=W, padx=60)
+tb1_fr1.grid(row=3, column=2, sticky=W,padx=180)
+#executar a janel4
 root.mainloop()
