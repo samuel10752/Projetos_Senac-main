62c90c3476e4f058e1c168b253552ef1e41a05c6
Samuel <samuelcauabarbosa114@gmail.com>
2022-06-13 18:36:45 -0300

    atualização 18:36 login

--- Projetos_Senac-main/tkinter/Cadastro/tela_login.py
+++ Projetos_Senac-main/tkinter/Cadastro/tela_login.py
@@ -1,27 +1,29 @@
 from tkinter import *
+from tkinter.ttk import Labelframe
 
 #criar janela
 
 root = Tk()
-root.title("Login de Funcionario ")
-fr1 = Frame(root)
+fr1 = LabelFrame(root, bg='green')
 
 #geometria
-root.geometry('380x150+720+400')
-#root.resizable(width=False, height=False)
+root.geometry('615x173+680+400')
+root.overrideredirect(True)
+root.resizable(width=False, height=False)
 
 #criar os widgets
 
 #---Frame 1---
-lb0_fr1 = Label(fr1, text='Usuario:', font="Arial 20")
-lb1_fr1 = Label(fr1, text='Senha:', font="Arial 20")
+lb0_fr1 = Label(fr1, text='Login do Funcionario', font='Arial 22',bg='green')
+lb1_fr1 = Label(fr1, text='Usuario:', font="Arial 20",bg='green')
+lb2_fr1 = Label(fr1, text='Senha:', font="Arial 20",bg='green')
 
 #--Entrada ---
-lb2_fr1 = Entry(fr1, font='Arial 18')
-lb3_fr1 = Entry(fr1, font='Arial 18')
+int0_fr1 = Entry(fr1, font='Arial 18', width=35)
+int1_fr1 = Entry(fr1, font='Arial 18', width=35)
 #--Button ---
-tb0_fr1 = Button(fr1,text='Entrar', font="Arial 20",width=5)
-tb1_fr1 = Button(fr1, text='Voltar', font="Arial 20",width=5)
+tb0_fr1 = Button(fr1,text='Entrar', font="Arial 20",width=15,bg='black', fg='white')
+tb1_fr1 = Button(fr1, text='Voltar', font="Arial 20",width=15,bg='black', fg='white')
 
 #---Configuração do Frame---
 fr1.grid()
@@ -31,15 +33,16 @@ fr1.grid()
 #organizar os widgets
 
 #---Frame 1---w
-lb0_fr1.grid(row=0, column=2,sticky=W, padx=5)
-lb1_fr1.grid(row=1, column=2,sticky=W, padx=5)
+lb0_fr1.grid(row=0,column=1,sticky=W,padx=150)
+lb1_fr1.grid(row=1, column=1,sticky=W, padx=5)
+lb2_fr1.grid(row=2, column=1,sticky=W, padx=5)
 
 #--Entrada --
-lb2_fr1.grid(row=0,column=2,sticky=W,padx=110)
-lb3_fr1.grid(row=1,column=2,sticky=W,padx=110)
+int0_fr1.grid(row=1,column=1,sticky=W,padx=110)
+int1_fr1.grid(row=2,column=1,sticky=W,padx=110)
 
 #---Butoon Função---
-tb0_fr1.grid(row=3, column=2, sticky=W, padx=60)
-tb1_fr1.grid(row=3, column=2, sticky=W,padx=180)
+tb0_fr1.grid(row=4, column=1, sticky=W, padx=55)
+tb1_fr1.grid(row=4, column=1, sticky=E,padx=110)
 #executar a janel4
 root.mainloop()
